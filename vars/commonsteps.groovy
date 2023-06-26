acbc6e63065a36e502baa2a50d927eaa207d3c2b
chineduebinaso <64101766+chineduebinaso@users.noreply.github.com>
2023-06-26 16:55:10 +0100

    Update commonsteps.groovy

--- vars/commonsteps.groovy
+++ vars/commonsteps.groovy
@@ -2,19 +2,22 @@ def step1() {
     echo 'Running Step 1'
     // Add your actual implementation code for Step 1 here
     // For example:
-    sh 'echo "This is Step 1"'
+    sh 'echo "sh "Build"'
+    sh "mvn clean package"
 }
 
 def step2() {
     echo 'Running Step 2'
     // Add your actual implementation code for Step 2 here
     // For example:
-    sh 'echo "This is Step 2"'
+    sh 'echo "Sonarqube report"'
+    sh "mvn clean sonar:sonar"
 }
 
 def step3() {
     echo 'Running Step 3'
     // Add your actual implementation code for Step 3 here
     // For example:
-    sh 'echo "This is Step 3"'
+    sh 'echo "Upload to Nexus"'
+    sh "mvn clean deploy"
 }
