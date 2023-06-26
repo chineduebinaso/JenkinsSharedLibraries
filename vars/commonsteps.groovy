2080f27f902535bb65b80bb2f80b019dabaf0238
chineduebinaso <chineduebinaso@yahoo.com>
2023-06-26 16:08:31 +0100

    first commit

--- vars/commonsteps.groovy
+++ vars/commonsteps.groovy
@@ -1,1 +1,20 @@
+def step1() {
+    echo 'Running Step 1'
+    // Add your actual implementation code for Step 1 here
+    // For example:
+    sh 'echo "This is Step 1"'
+}
 
+def step2() {
+    echo 'Running Step 2'
+    // Add your actual implementation code for Step 2 here
+    // For example:
+    sh 'echo "This is Step 2"'
+}
+
+def step3() {
+    echo 'Running Step 3'
+    // Add your actual implementation code for Step 3 here
+    // For example:
+    sh 'echo "This is Step 3"'
+}
